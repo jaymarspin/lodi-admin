590bdce97b5e6d93eecbe53500b3fc2dd28376df
jaymar daligdig <jaydaligdig123@gmail.com>
2021-08-16 22:56:23 +0800

    yeah

--- admin/src/app/pages/admin-home/fan/fan.component.ts
+++ admin/src/app/pages/admin-home/fan/fan.component.ts
@@ -45,146 +45,25 @@ export class FanComponent implements OnInit {
 
 
 
-    filter:any
+
 
    usershow:boolean = false
   
-   categories:any = Array()
+
    userType:any
 
-   filtergo:boolean = false
-
-   teaser:any
 
 
-
-   afuConfig = {
-    multiple: false,
-    formatsAllowed: ".mp4,.png",
-    
-    maxSize: "50",
-    uploadAPI:  {
-      url:this.http.server+"teaser-upload.php",
-      method:"POST",
-      headers: {
-     "Content-Type" : "text/plain;charset=UTF-8", 
-      },
-      params: {
-         
-      },
-      responseType: 'stringify',
-    },
-    theme: "dragNDrop",
-    hideProgressBar: false,
-    hideResetBtn: true,
-    hideSelectBtn: true,
-    fileNameIndex: true,
-    replaceTexts: {
-      selectFileBtn: 'Select Files',
-      resetBtn: 'Reset',
-      uploadBtn: 'Upload',
-      dragNDropBox: 'Drag N Drop',
-      attachPinBtn: 'Attach Files...',
-      afterUploadMsg_success: 'Successfully Uploaded !',
-      afterUploadMsg_error: 'Upload Failed !',
-      sizeLimit: 'Size Limit'
-    }
-};
-
+ 
 resetVar:boolean = false
 caching:any
-
-teaserplay:any
  
-private fileUpload1:  AngularFileUploaderComponent;
   constructor(private cd: ChangeDetectorRef,
     private zone: NgZone,private router: Router,public service: ServiceService,public http: HttpRequestService) {
     this.page = 1
     this.limit = 50
     this.pagebtn = Array()
-    this.filter = Array()
-
-
-    this.categories.push({
-      title: "Actor",
-      value: "actor",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Actress",
-      value: "actress",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Youtuber",
-      value: "youtuber",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Singer",
-      value: "singer",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Rapper",
-      value: "rapper",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "TV Host",
-      value: "tvhost",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "News Anchor",
-      value: "newsanchor",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Media",
-      value: "media",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "DJ",
-      value: "dj",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Athletes",
-      value: "athletes",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Streamer",
-      value: "streamer",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Influencer",
-      value: "influencer",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Politicians",
-      value: "politicians",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Spiritual",
-      value: "spiritual",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Wealth Coach",
-      value: "wealthcoach",
-      css: "unselected"
-    })
-    this.categories.push({
-      title: "Business Tycoon",
-      value: "businesstycon",
-      css: "unselected"
-    })
+   
 
    }
   ngOnInit(): void {
@@ -206,18 +85,10 @@ private fileUpload1:  AngularFileUploaderComponent;
 
 
   }
-  getBase64Image(img) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    var dataURL = canvas.toDataURL("image/png");
-    return dataURL;
-  }
+   
 
   cached(item){
-    delete(this.teaserplay)
+
     console.log(item)
     this.fname = item.fname
     this.lname = item.lname
@@ -232,13 +103,7 @@ private fileUpload1:  AngularFileUploaderComponent;
     
 
     this.cd.markForCheck();
-    setTimeout(() =>{
-      this.caching = item
-    if(item.teaser.length > 0){
-      this.teaserplay = item.teaser[0].teaser
-    } 
-      this.cd.detectChanges()
-    },100)
+  
      
     
   }
@@ -291,8 +156,8 @@ private fileUpload1:  AngularFileUploaderComponent;
     this.getdata(this.page)
     this.searchpass = false
     delete(this.search)
-    delete(this.filterarray)
-    this.filtergo = false
+ 
+
   }
 
   getdata(pager){
@@ -364,152 +229,84 @@ delete(id){
  
 
  
+
  
- 
-// rtpcr(){
-//   this.router.navigate(["admin-home/"])
-// }
-
-
-updateLodi(){
-  let data = {
-    fname: this.fname,
-    lname: this.lname,
-    email: this.email,
-    contact: this.contact,
-    social: this.social,
-    followers: this.followers,
-    id: this.updateid,
-    active: this.approved,
-    category: this.category
-
-  }
-
-  this.http.postData("update-lodi.php",data).subscribe(res =>{
-    let result = res.json()
-    if(result.message == "success"){
-    delete(this.filterarray)
-      this.refreshed()
-    }
-  })
-
-}
-filterarray:any = Array()
-cachedfilter(e,i){
-   
-
-  if(this.categories[i].css == "unselected"){
-    this.categories[i].css = 'selected'
-  }else{
-    this.categories[i].css = 'unselected'
-  }
-  delete(this.filterarray)
-  this.filterarray = Array()
-  for(var ii = 0;ii < this.categories.length;ii++){
-    if(this.categories[ii].css == "selected"){
-      this.filterarray.push(this.categories[ii].value)
-    }
-    
-  }
-
-  console.log(this.filterarray)
-}
-
-
-filternow(){
-  this.filtergo = true
-  if(this.filterarray.length > 0){  
-    let data = {
-      filterarray: this.filterarray
-  }
-
-  this.http.postData("filterlodi.php",data).subscribe(res =>{
-    this.lodis = res.json()
-    this.searchpass = true
-  })
-  }else{
-    this.refreshed()
-  }
-
-
-  
-  
-}
  
 cacheData(item){
   console.log(item)
   this.caching = item
 }
-uploadteaser(){
-
- 
-  //  $.post( this.http.server+"teaser-upload.php",{ "video": $("#video").val() },  res =>{
-  //   console.log(res)
-  // })
-
- 
-  let data = {
-    teaser: this.teaserarr[0],
-    id: this.caching.id
-  }
-  this.http.postData("teaser-upload.php",data).subscribe(res =>{
-    console.log(res)
+inactivate(item){
+  console.log(item.id)
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const data ={
+        id: item.id
+      }
+      console.log(data)
+      this.http.postData(`inactivate-fan.php`,data).subscribe({
+        next: data =>{
+          console.log(data)
+          const result = data.json()
+          if(result.message === 'success'){
+            Swal.fire(
+              'Good job!',
+              'Successfully inactivated!',
+              'success'
+            ).then(() =>{
+              this.getdata(this.page)
+            })
+            
+          }else{
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: `${result.message}`,
+              footer: ''
+            })
+          }
+        },error: err =>{
+          console.log(err)
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `${err}`,
+            footer: ''
+          })
+        }
+      })
+    }
   })
 
-
-  }
-
- getBase64(e) {
-  var file = e.target.files[0];
-    let reader = new FileReader();
-    reader.onload = (e) => {
-    let image = e.target.result;
-    console.log(image);
-    };
-  reader.readAsDataURL(file);
-  
 }
-teaserarr:any = new Array()
-handleUpload(event) {
-  $(".harang").css({display: "none"})
-
-  $(".fileinput-remove, fileinput-remove-button").click(() =>{
-   $(".harang").css({display: "block"})
+transactions: any;
+transactionsCount: any;
+gettransactions(item){
+  this.http.getData(`get-fan-transactions.php?id=${item.id}`).subscribe({
+    next: data =>{
+      setTimeout(() =>{
+        const result = data.json()
+      this.transactionsCount = result.transactions_count
+      this.transactions = result.transactions
     
+        this.cd.detectChanges()
+      },1000)
+      
+      
+      
+    },error: err =>{
+      console.log(err)
+    }
   })
-   
- for(var i =0;i < event.target.files.length;i++){
-    
-   const file = event.target.files[i];
-   const reader = new FileReader();
-   reader.readAsDataURL(file);
-   reader.onload = () => {
-       console.log
-       this.teaserarr.push({
-         base64: reader.result,
-         name: file.name
-       }) 
-       
-       
-   };
-
-   console.log(this.teaserarr)
-
-   setInterval((e) =>{
-     $(".kv-zoom-thumb").css({display: "none"}) 
-   },500)
-  
-    
-  
-   
- }
-
- 
- 
- 
 }
-
-
 }
 
 
